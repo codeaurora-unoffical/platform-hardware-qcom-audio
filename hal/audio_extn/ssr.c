a81334e8dfa95f15c6bbb8e4e9d60890da5eb418
Arun Kumar Dasari <Arun@codeaurora.org>
2016-07-28 20:36:45 +0530

    hal: changes to support 24 bit record
    Changes to support 24 bit record if input format request is
    AUDIO_FORMAT_PCM_8_24_BIT or AUDIO_FORMAT_PCM_24_BIT_PACKED
    
    Change-Id: I7cda46910d362e99894e81afda4eef1623cc7a12

--- hal/audio_extn/ssr.c
+++ hal/audio_extn/ssr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
  * Copyright (C) 2013 The Android Open Source Project
@@ -348,7 +348,8 @@ int audio_extn_ssr_check_and_set_usecase(struct stream_in *in)
     if (audio_extn_ssr_get_enabled() &&
            ((channel_count == 2) || (channel_count == 6)) &&
            ((AUDIO_SOURCE_MIC == source) || (AUDIO_SOURCE_CAMCORDER == source)) &&
-           ((AUDIO_DEVICE_IN_BUILTIN_MIC == devices) || (AUDIO_DEVICE_IN_BACK_MIC == devices))) {
+           ((AUDIO_DEVICE_IN_BUILTIN_MIC == devices) || (AUDIO_DEVICE_IN_BACK_MIC == devices)) &&
+           (in->format == AUDIO_FORMAT_PCM_16_BIT)) {
 
         ALOGD("%s: Found SSR use case starting SSR lib with channel_count :%d",
                       __func__, channel_count);
