c877915b8ac627ebcc0554214080beefd0e19d29
Derek Chen <chenche@codeaurora.org>
2017-03-26 18:54:11 -0400

    configs: add audio input/output policy config
    Add audio input/output (io) policy config file.
    This is to combine individual config files and
    remove staging of output policy config.
    
    Change-Id: I1a3a88654dcecc7fb558fd96edd5a86a293b783f

--- configs/msmnile_au/msmnile_au.mk
+++ configs/msmnile_au/msmnile_au.mk
@@ -87,7 +87,7 @@ endif
 DEVICE_PACKAGE_OVERLAYS += hardware/qcom/audio/configs/msmnile_au/overlay
 
 PRODUCT_COPY_FILES += \
-    hardware/qcom/audio/configs/msmnile_au/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
+    hardware/qcom/audio/configs/msmnile_au/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     hardware/qcom/audio/configs/msmnile_au/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     hardware/qcom/audio/configs/msmnile_au/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     hardware/qcom/audio/configs/msmnile_au/mixer_paths_adp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_adp.xml \
