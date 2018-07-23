fbd0fe7db48c08b4243dcf94cf735cc018c5afef
Timothy Sham <tsham@codeaurora.org>
2018-07-23 16:50:45 -0400

    hal: Add support for Rx DTMF Tone Generation and Detection
    Add support through audio stream output set parameters for RX DTMF
    tone generation and detection for voice. DTMF detection
    uses ADSP handler functions to receive callback events from mixer
    events.
    
    Change-Id: I257c3cf82fcac58bb6c38cce83fa7a54a084bbcb

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -3692,6 +3692,29 @@ static int get_alive_usb_card(struct str_parms* parms) {
     return -ENODEV;
 }
 
+static int out_set_callback(struct audio_stream_out *stream,
+            stream_callback_t callback, void *cookie)
+{
+    struct stream_out *out = (struct stream_out *)stream;
+    int ret;
+
+    ALOGV("%s", __func__);
+    lock_output_stream(out);
+    out->client_callback = callback;
+    out->client_cookie = cookie;
+    if (out->adsp_hdlr_stream_handle) {
+        ret = audio_extn_adsp_hdlr_stream_set_callback(
+                                out->adsp_hdlr_stream_handle,
+                                callback,
+                                cookie);
+        if (ret)
+            ALOGE("%s:adsp hdlr callback registration failed %d",
+                   __func__, ret);
+    }
+    pthread_mutex_unlock(&out->lock);
+    return 0;
+}
+
 static int out_set_parameters(struct audio_stream *stream, const char *kvpairs)
 {
     struct stream_out *out = (struct stream_out *)stream;
@@ -3814,6 +3837,7 @@ static int out_set_parameters(struct audio_stream *stream, const char *kvpairs)
                 if (!voice_is_call_state_active(adev)) {
                     if (adev->mode == AUDIO_MODE_IN_CALL) {
                         adev->current_call_output = out;
+                        out->stream.set_callback = out_set_callback;
                         ret = voice_start_call(adev);
                     }
                 } else {
@@ -3865,9 +3889,13 @@ static int out_set_parameters(struct audio_stream *stream, const char *kvpairs)
     routing_fail:
 
     if (out == adev->primary_output) {
+        lock_output_stream(out);
         pthread_mutex_lock(&adev->lock);
         audio_extn_set_parameters(adev, parms);
+        if (voice_is_call_state_active(adev))
+            voice_extn_out_set_parameters(out, parms);
         pthread_mutex_unlock(&adev->lock);
+        pthread_mutex_unlock(&out->lock);
     }
     if (is_offload_usecase(out->usecase)) {
         lock_output_stream(out);
@@ -4886,29 +4914,6 @@ static int out_get_presentation_position(const struct audio_stream_out *stream,
     return ret;
 }
 
-static int out_set_callback(struct audio_stream_out *stream,
-            stream_callback_t callback, void *cookie)
-{
-    struct stream_out *out = (struct stream_out *)stream;
-    int ret;
-
-    ALOGV("%s", __func__);
-    lock_output_stream(out);
-    out->client_callback = callback;
-    out->client_cookie = cookie;
-    if (out->adsp_hdlr_stream_handle) {
-        ret = audio_extn_adsp_hdlr_stream_set_callback(
-                                out->adsp_hdlr_stream_handle,
-                                callback,
-                                cookie);
-        if (ret)
-            ALOGW("%s:adsp hdlr callback registration failed %d",
-                   __func__, ret);
-    }
-    pthread_mutex_unlock(&out->lock);
-    return 0;
-}
-
 static int out_pause(struct audio_stream_out* stream)
 {
     struct stream_out *out = (struct stream_out *)stream;
@@ -5867,6 +5872,7 @@ int adev_open_output_stream(struct audio_hw_device *dev,
     out->hal_output_suspend_supported = 0;
     out->dynamic_pm_qos_config_supported = 0;
     out->set_dual_mono = false;
+    out->rx_dtmf_tone_gain = 0;
 
     if ((flags & AUDIO_OUTPUT_FLAG_BD) &&
         (property_get_bool("vendor.audio.matrix.limiter.enable", false)))
--- hal/voice_extn/voice_extn.h
+++ hal/voice_extn/voice_extn.h
@@ -20,6 +20,8 @@
 #ifndef VOICE_EXTN_H
 #define VOICE_EXTN_H
 
+#include "adsp_hdlr.h"
+
 #ifdef MULTI_VOICE_SESSION_ENABLED
 int voice_extn_start_call(struct audio_device *adev);
 int voice_extn_stop_call(struct audio_device *adev);
@@ -29,6 +31,8 @@ int voice_extn_get_session_from_use_case(struct audio_device *adev,
 void voice_extn_init(struct audio_device *adev);
 int voice_extn_set_parameters(struct audio_device *adev,
                               struct str_parms *parms);
+int voice_extn_out_set_parameters(struct stream_out *out,
+                                  struct str_parms *parms);
 void voice_extn_get_parameters(const struct audio_device *adev,
                                struct str_parms *query,
                                struct str_parms *reply);
@@ -66,6 +70,12 @@ static void __unused voice_extn_init(struct audio_device *adev __unused)
 
 static int __unused voice_extn_set_parameters(struct audio_device *adev __unused,
                                      struct str_parms *parms __unused)
+{
+    return -ENOSYS;
+}
+
+static int __unused voice_extn_out_set_parameters(struct stream_out *out __unused,
+                                  struct str_parms *parms __unused)
 {
     return -ENOSYS;
 }
@@ -282,6 +292,55 @@ static bool __unused voice_extn_compress_voip_is_started(
 {
     ALOGV("%s: COMPRESS_VOIP_ENABLED is not defined", __func__);
     return false;
+}
+
+#endif
+
+#ifdef DTMF_ENABLED
+int voice_extn_dtmf_generate_rx_tone(struct stream_out *out,
+                                     uint32_t dtmf_low_freq,
+                                     uint32_t dtmf_high_freq);
+
+int voice_extn_dtmf_set_rx_tone_gain(struct stream_out *out,
+                                     int32_t gain);
+
+int voice_extn_dtmf_set_rx_tone_off(struct stream_out *out);
+
+int voice_extn_dtmf_set_rx_detection(struct stream_out *out,
+                                     uint32_t session_id,
+                                     bool enable);
+#else
+static int __unused voice_extn_dtmf_generate_rx_tone(
+                            struct stream_out *out __unused,
+                            uint32_t dtmf_low_freq __unused,
+                            uint32_t dtmf_high_freq __unused)
+{
+    ALOGV("%s: DTMF_ENABLED is not defined", __func__);
+    return -ENOSYS;
+}
+
+static int __unused voice_extn_dtmf_set_rx_tone_gain(
+                            struct stream_out *out __unused,
+                            int32_t gain __unused)
+{
+    ALOGV("%s: DTMF_ENABLED is not defined", __func__);
+    return -ENOSYS;
+}
+
+static int __unused voice_extn_dtmf_set_rx_tone_off(
+                            struct stream_out *out __unused)
+{
+    ALOGV("%s: DTMF_ENABLED is not defined", __func__);
+    return -ENOSYS;
+}
+
+static int __unused voice_extn_dtmf_set_rx_detection(
+                            struct stream_out *out __unused,
+                            uint32_t session_id __unused,
+                            bool enable __unused)
+{
+    ALOGV("%s: DTMF_ENABLED is not defined", __func__);
+    return -ENOSYS;
 }
 
 #endif
