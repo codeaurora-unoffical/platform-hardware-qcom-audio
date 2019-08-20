445b1f9a48a204770fae551fbbb187157921c9d6
Gangadhar S <gangadha@codeaurora.org>
2019-08-20 19:32:18 +0530

    hal: Update Dolby licensing mechanism
    Initialize ip_hdlr interface before stream open
    ip_hdlr interface change to handle device switch scenario.
    ip_hdlr interface change to handle security on active stream.
    
    Change-Id: Ia6322be431d8ca4907e594d84d7b2a79993885fa

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -318,6 +318,9 @@ struct stream_inout {
     struct audio_device *dev;
     void *adsp_hdlr_stream_handle;
     void *ip_hdlr_handle;
+    bool adm_event_enable;
+    bool asm_event_enable;
+    bool ip_hdlr_enabled;
     stream_callback_t client_callback;
     void *client_cookie;
     audio_input_flags_t input_flags;
@@ -364,6 +367,9 @@ struct stream_out {
 
     void *adsp_hdlr_stream_handle;
     void *ip_hdlr_handle;
+    bool adm_event_enable;
+    bool asm_event_enable;
+    bool ip_hdlr_enabled;
 
     stream_callback_t client_callback;
     void *client_cookie;
@@ -643,6 +649,10 @@ struct audio_device {
     struct pcm_params *use_case_table[AUDIO_USECASE_MAX];
     struct listnode audio_patch_record_list;
     unsigned int audio_patch_index;
+    void *ip_hdlr_handle;
+    int ip_hdlr_asm_cnt;
+    int ip_hdlr_adm_cnt;
+
 };
 
 struct audio_patch_record {
