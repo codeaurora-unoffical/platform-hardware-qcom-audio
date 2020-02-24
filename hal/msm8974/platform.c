f9d9d8932aa0c98dfccfb025e379d18a46080b16
Pallavi <pallavim@codeaurora.org>
2020-02-24 22:25:27 +0530

    hal: add support for ecall
    add ecall device support for auto dsda.
    
    Change-Id: I64fbc8bf6f5ac1f151260b1a2df873b3e122d332

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
  * Not a Contribution.
@@ -584,6 +585,8 @@ static const char * const device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_OUT_SPEAKER2] = "speaker2",
     [SND_DEVICE_OUT_SPEAKER3] = "speaker3",
     [SND_DEVICE_OUT_VOICE_DL_TX] = "voice-dl-tx",
+    [SND_DEVICE_OUT_ECALL] = "handset",
+
 
     /* Capture sound devices */
     [SND_DEVICE_IN_HANDSET_MIC] = "handset-mic",
@@ -706,6 +709,7 @@ static const char * const device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_IN_HANDSET_GENERIC_QMIC_AND_EC_REF_LOOPBACK] = "handset-qmic-and-ec-ref-loopback",
     [SND_DEVICE_IN_HANDSET_GENERIC_6MIC_AND_EC_REF_LOOPBACK] = "handset-6mic-and-ec-ref-loopback",
     [SND_DEVICE_IN_HANDSET_GENERIC_8MIC_AND_EC_REF_LOOPBACK] = "handset-8mic-and-ec-ref-loopback",
+    [SND_DEVICE_IN_ECALL] = "handset-mic",
 };
 
 // Platform specific backend bit width table
@@ -822,6 +826,7 @@ static int acdb_device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_OUT_BUS_PHN] = 94,
     [SND_DEVICE_OUT_SPEAKER2] = 14,
     [SND_DEVICE_OUT_SPEAKER3] = 14,
+    [SND_DEVICE_OUT_ECALL] = 7,
     [SND_DEVICE_IN_HANDSET_MIC] = 4,
     [SND_DEVICE_IN_HANDSET_MIC_EXTERNAL] = 4,
     [SND_DEVICE_IN_HANDSET_MIC_AEC] = 106,
@@ -926,6 +931,7 @@ static int acdb_device_table[SND_DEVICE_MAX] = {
     [SND_DEVICE_IN_LINE] = 4,
     [SND_DEVICE_IN_EC_REF_LOOPBACK_QUAD] = 4,
     [SND_DEVICE_IN_BUS] = 11,
+    [SND_DEVICE_IN_ECALL] = 4,
 };
 
 struct name_to_index {
