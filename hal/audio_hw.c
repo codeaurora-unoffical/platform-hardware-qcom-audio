55ab209d7efd31fce6a1774fc7e66147d8e5c4f4
Asish Bhattacharya <asishb@codeaurora.org>
2020-07-21 07:34:12 +0530

    qahw: enable SSR callback registration
    Change-Id: Id5d57345496f47565efa6314f639b827d6089cf2

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -7499,6 +7499,8 @@ static void adev_snd_mon_cb(void *cookie, struct str_parms *parms)
             adev->card_status = status;
             platform_snd_card_update(adev->platform, status);
             audio_extn_fm_set_parameters(adev, parms);
+            if(adev->ssr_callback)
+                adev->ssr_callback(status, adev->ssr_callback_payload);
             ALOGE("%s: Card_status (%d) in the list",
                                    __func__, status);
         } else if (is_ext_device_status) {
@@ -7584,6 +7586,15 @@ int check_a2dp_restore(struct audio_device *adev, struct stream_out *out, bool r
     return ret;
 }
 
+static int adev_set_ssr_callback(struct audio_hw_device *dev, ssr_callback_t cb_func, void *priv)
+{
+    struct audio_device *adev = (struct audio_device *)dev;
+
+    adev->ssr_callback = cb_func;
+    adev->ssr_callback_payload = priv;
+    return 0;
+}
+
 static int adev_open(const hw_module_t *module, const char *name,
                      hw_device_t **device)
 {
@@ -7641,6 +7652,7 @@ static int adev_open(const hw_module_t *module, const char *name,
     adev->device.get_audio_port = adev_get_audio_port;
     adev->device.set_audio_port_config = adev_set_audio_port_config;
     adev->device.dump = adev_dump;
+    adev->device.set_ssr_callback = adev_set_ssr_callback;
 
     /* Set the default route before the PCM stream is opened */
     adev->mode = AUDIO_MODE_NORMAL;
