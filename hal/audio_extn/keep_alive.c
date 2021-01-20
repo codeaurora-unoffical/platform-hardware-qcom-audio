d898846efce87384cf6c347c40dc52b7500b30f8
Gangadhar S <gangadha@codeaurora.org>
2021-01-20 18:50:39 +0530

    audio-hal: Conditional memory free in keep_alive
    Issue: Crash seen in keep_alive.
    
    Root cause: Double freeing of pcm structure in keep_alive
                causing crash in keep_alive.
    Fix: Null check before free of pcm structure.
    
    Change-Id: Ifbc37f4ef192d0ee6aca03fe6cc3e3f9816c15cf

--- hal/audio_extn/keep_alive.c
+++ hal/audio_extn/keep_alive.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+* Copyright (c) 2014-2018, 2020-2021 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -359,8 +359,10 @@ static int keep_alive_cleanup()
         list_remove(&uc_info->list);
         free(uc_info);
     }
-    pcm_close(ka.pcm);
-    ka.pcm = NULL;
+    if (ka.pcm != NULL) {
+        pcm_close(ka.pcm);
+        ka.pcm = NULL;
+    }
     ka.active_devices = KEEP_ALIVE_OUT_NONE;
     return 0;
 }
